4053baa698f1047f09540c9ff3d6e8a08d844335
Matt Hickford <matt.hickford@gmail.com>
2014-05-29 23:58:48 +0100

    Comment example

--- example.py
+++ example.py
@@ -19,5 +19,12 @@ login_form = login_page.soup.select("#login")[0].select("form")[0]
 login_form.select("#login_field")[0]['value'] = args.username
 login_form.select("#password")[0]['value'] = args.password
 
+# submit!
 page2 = browser.submit(login_page.response, login_form)
+
+# verify we are now logged in
 assert page2.soup.select(".logout-form")
+
+# verify we remain logged in (thanks to cookies) as we browse the rest of the site
+page3 = browser.get("https://github.com/matt-hickford/MechanicalSoup")
+assert page3.soup.select(".logout-form")
