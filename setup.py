3a6dcc275dc65e051c0b71887919054be6ad32c6
Matt Hickford <matt.hickford@gmail.com>
2014-05-30 19:11:07 +0100

    package project

---
+++ setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+from codecs import open  # To use a consistent encoding
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='MechanicalSoup',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # http://packaging.python.org/en/latest/tutorial.html#version
+    version='0.01',
+
+    description='A Python library for automating interaction with websites',
+
+    # The project's main homepage.
+    url='https://github.com/matt-hickford/MechanicalSoup',
+
+    # Author details
+    author='Matt Hickford',
+    author_email='matt.hickford@gmail.com',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+    ],
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=['mechanicalsoup'],
+
+    # List run-time dependencies here.  These will be installed by pip when your
+    # project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
+    install_requires=['requests', 'beautifulsoup4'],
+)
