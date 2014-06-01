022d8b8e1da4b53eb85737b7728214a3840a51f1
Matt Hickford <matt.hickford@gmail.com>
2014-06-01 23:13:00 +0100

    version 0.0.2

--- setup.py
+++ setup.py
@@ -7,35 +7,26 @@ here = path.abspath(path.dirname(__file__))
 setup(
     name='MechanicalSoup',
 
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # http://packaging.python.org/en/latest/tutorial.html#version
-    version='0.01',
+    version='0.0.2',
 
     description='A Python library for automating interaction with websites',
 
-    # The project's main homepage.
     url='https://github.com/matt-hickford/MechanicalSoup',
 
-    # Author details
     author='Matt Hickford',
     author_email='matt.hickford@gmail.com',
 
-    # Choose your license
     license='MIT',
 
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
     ],
 
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
     packages=['mechanicalsoup'],
 
     # List run-time dependencies here.  These will be installed by pip when your
