ee34e666db1c816af54e6a1fdc4544dc10385405
Matt Hickford <matt.hickford@gmail.com>
2014-06-29 14:40:56 +0100

    version 0.2.0

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ here = path.abspath(path.dirname(__file__))
 setup(
     name='MechanicalSoup',
 
-    version='0.1.2',
+    # useful: python setup.py sdist bdist_wheel upload
+    version='0.2.0',
 
     description='A Python library for automating interaction with websites',
 
