19779564822f91884d4104d01f7b7465b45152e3
Mirth Hickford <mirth.hickford@gmail.com>
2015-02-08 00:22:40 +0000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'MechanicalSoup'
-copyright = '2014, Matt Hickford'
+copyright = '2014'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -198,7 +198,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   ('index', 'MechanicalSoup.tex', 'MechanicalSoup Documentation',
-   'Matt Hickford', 'manual'),
+   '', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -228,7 +228,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'mechanicalsoup', 'MechanicalSoup Documentation',
-     ['Matt Hickford'], 1)
+     [''], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -242,7 +242,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', 'MechanicalSoup', 'MechanicalSoup Documentation',
-   'Matt Hickford', 'MechanicalSoup', 'One line description of project.',
+   '', 'MechanicalSoup', 'One line description of project.',
    'Miscellaneous'),
 ]
 
