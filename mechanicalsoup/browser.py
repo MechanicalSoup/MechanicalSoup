d56af3f9b7459496dfd4670c075a3b9baaecfffc
Xuefeng Zhu <xzhu15@illinois.edu>
2015-06-29 11:22:11 -0700

    Replace ' with " to make style more consistent

--- mechanicalsoup/browser.py
+++ mechanicalsoup/browser.py
@@ -12,7 +12,7 @@ class Browser:
 
     @staticmethod
     def add_soup(response):
-        if 'text/html' in response.headers.get('Content-Type', ''):
+        if "text/html" in response.headers.get("Content-Type", ""):
             try:
                 response.soup = bs4.BeautifulSoup(response.content)
             except:
@@ -34,56 +34,56 @@ class Browser:
         return response
 
     def _build_request(self, form, url=None, **kwargs):
-        method = form['method']
-        action = form['action']
+        method = form["method"]
+        action = form["action"]
         url = urllib.parse.urljoin(url, action)
 
         # read http://www.w3.org/TR/html5/forms.html
-        data = kwargs.get('data') or dict()
-        files = kwargs.get('files') or dict()
+        data = kwargs.get("data") or dict()
+        files = kwargs.get("files") or dict()
 
         for input in form.select("input"):
-            name = input.get('name')
+            name = input.get("name")
             # web browsers use empty string for inputs with missing values
-            value = input.get('value', '')
+            value = input.get("value", "")
             if not name:
                 continue
 
-            if input.get('type') in ('radio', 'checkbox') and 'checked' not in input.attrs:
+            if input.get("type") in ("radio", "checkbox") and "checked" not in input.attrs:
                 continue
 
-            if input.get('type') == 'checkbox':
+            if input.get("type") == "checkbox":
                 if not name in data:
                     data[name] = list()
                 data[name].append(value)
 
-            elif input.get('type') == 'file':
+            elif input.get("type") == "file":
                 # read http://www.cs.tut.fi/~jkorpela/forms/file.html
-                # in web browsers, file upload only happens if the form's (or
-                # submit button's) enctype attribute is set to
-                # 'multipart/form-data'. we don't care, simplify.
+                # in web browsers, file upload only happens if the form"s (or
+                # submit button"s) enctype attribute is set to
+                # "multipart/form-data". we don"t care, simplify.
                 if not value:
                     continue
                 if isinstance(value, string_types):
-                    value = open(value, 'rb')
+                    value = open(value, "rb")
                 files[name] = value
 
             else:
                 data[name] = value
 
         for textarea in form.select("textarea"):
-            name = textarea.get('name')
+            name = textarea.get("name")
             if not name:
                 continue
             data[name] = textarea.text
 
         for select in form.select("select"):
-            name = select.get('name')
+            name = select.get("name")
             if not name:
                 continue
             for i, option in enumerate(select.select("option")):
                 if i == 0 or "selected" in option.attrs:
-                    data[name] = option.get('value', '')
+                    data[name] = option.get("value", "")
 
         return requests.Request(method, url, data=data, files=files, **kwargs)
 
