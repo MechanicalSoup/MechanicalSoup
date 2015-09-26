19a9e4c8a4afd8edd7a0d5c9aa0d34418ff05dc2
Mirth Hickford <mirth.hickford@gmail.com>
2015-09-26 10:39:57 +0100

    python 3.5

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
 
     packages=['mechanicalsoup'],
