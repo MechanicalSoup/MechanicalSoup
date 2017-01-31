d995a2d38add5ef99481c6c6e51e87ca8b647fc2
J.Townsend <L1ghtn1ng@users.noreply.github.com>
2017-01-31 21:28:00 +0000

    add py3.6 to setup.py

--- setup.py
+++ setup.py
@@ -22,13 +22,12 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
     ],
 
     packages=['mechanicalsoup'],
