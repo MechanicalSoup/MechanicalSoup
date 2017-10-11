b73687d36d2fb0a6d4a9ca4a8325fdebb71764a6
Matthieu Moy <git@matthieu-moy.fr>
2017-10-11 14:22:41 +0200

    docs/conf.py: import local mechanicalsoup, not the installed one

--- docs/conf.py
+++ docs/conf.py
@@ -16,12 +16,13 @@
 import sys
 import os
 
-import mechanicalsoup
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
+
+import mechanicalsoup
 
 # -- General configuration ------------------------------------------------
 
