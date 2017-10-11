30f67ea63e34179f58aa9dd466936090c1326ee0
Matthieu Moy <git@matthieu-moy.fr>
2017-10-11 13:46:22 +0200

    enable autodoc with Sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ import mechanicalsoup
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ['sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
