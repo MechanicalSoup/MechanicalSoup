57579fcf38bdc336a0c63d6abe4121e83e26ee15
Matthieu Moy <git@matthieu-moy.fr>
2017-10-13 14:03:14 +0200

    Document LinkNotFoundError

--- mechanicalsoup/utils.py
+++ mechanicalsoup/utils.py
@@ -1,2 +1,15 @@
 class LinkNotFoundError(BaseException):
+    """Exception raised when mechanicalsoup fails to find something.
+
+    This happens in situations like (non-exhaustive list):
+
+    * :func:`~mechanicalsoup.StatefulBrowser.find_link` is called, but no link is found.
+
+    * The browser was configured with raise_on_404=True and a 404
+      error is triggered while browsing.
+
+    * The user tried to fill-in a field which doesn't exist in a form
+      (e.g. browser["name"] = "val" with browser being a
+      StatefulBrowser).
+    """
     pass
