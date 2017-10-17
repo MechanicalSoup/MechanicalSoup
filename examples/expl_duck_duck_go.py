5a8724e9baf6354d2bcae016838d90f82bc65a5b
Matthieu Moy <git@matthieu-moy.fr>
2017-10-17 21:45:02 +0200

    Examples with Google and DuckDuckGo

---
+++ examples/expl_duck_duck_go.py
@@ -0,0 +1,17 @@
+"""Example usage of MechanicalSoup to get the results from
+DuckDuckGo."""
+
+import mechanicalsoup
+
+# Connect to duckduckgo
+browser = mechanicalsoup.StatefulBrowser()
+browser.open("https://duckduckgo.com/")
+
+# Fill-in the search form
+browser.select_form('#search_form_homepage')
+browser["q"] = "MechanicalSoup"
+browser.submit_selected()
+
+# Display the results
+for l in browser.get_current_page().select('a.result__a'):
+    print(l.text, '->', l.attrs['href'])
