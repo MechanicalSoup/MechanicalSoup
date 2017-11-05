6f6290190f7103c7a3cc68a7b18bda25f874491e
Matthieu Moy <git@matthieu-moy.fr>
2017-11-05 19:45:53 +0100

    setup.py: fix links to GitHub pages

--- mechanicalsoup/__version__.py
+++ mechanicalsoup/__version__.py
@@ -1,5 +1,6 @@
 __title__ = 'MechanicalSoup'
 __description__ = 'A Python library for automating interaction with websites'
 __url__ = 'https://mechanicalsoup.readthedocs.io/'
+__github_url__ = 'https://github.com/MechanicalSoup/MechanicalSoup'
 __version__ = '0.9.0'
 __license__ = 'MIT'
--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     version=about['__version__'],
 
     description=about['__description__'],
-    long_description=read('README.rst', about['__url__']),
+    long_description=read('README.rst', about['__github_url__']),
     url=about['__url__'],
 
     license=about['__license__'],
