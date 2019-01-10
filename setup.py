f9e8df743c1842dbb65322eaa1eccfb51edd2a3e
Dan Hemberger <daniel.hemberger@gmail.com>
2019-01-09 22:50:32 -0800

    Officially support python 3.7
    Add tests against python 3.7 in .travis.yml.
    
    Note that we can no longer use the default dist, "trusty", because
    python 3.7 requires an SSL package that is only available as of
    xenial. See https://github.com/travis-ci/travis-ci/issues/9815.
    
    On xenial, the PyPy packages have a different naming convention
    (e.g. just `pypy` and `pypy2.7` don't exist), so we switch to the
    naming convention for the most recent v6.0 PyPy release.
    See https://travis-ci.community/t/pypy-2-7-on-xenial/889.

--- setup.py
+++ setup.py
@@ -60,6 +60,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 
     packages=['mechanicalsoup'],
