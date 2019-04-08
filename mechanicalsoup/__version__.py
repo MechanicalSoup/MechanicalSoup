50643c992b577374fdb94f16559c42fb519ead99
Sanchez Quentin <quentin.sanchez--brochet@etu.univ-lyon1.fr>
2019-04-08 10:57:59 +0200

    Fixes broken image link in pypi documentation
        fixes issue #239
    
    Modify the relative image path to an absolute github URL.

--- mechanicalsoup/__version__.py
+++ mechanicalsoup/__version__.py
@@ -4,3 +4,5 @@ __url__ = 'https://mechanicalsoup.readthedocs.io/'
 __github_url__ = 'https://github.com/MechanicalSoup/MechanicalSoup'
 __version__ = '1.0.0-dev'
 __license__ = 'MIT'
+__github_assets_absoluteURL__ = """\
+https://raw.githubusercontent.com/MechanicalSoup/MechanicalSoup/master"""
