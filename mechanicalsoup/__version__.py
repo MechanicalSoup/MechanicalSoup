85b17b49b8dd11aa2a98cd18d43e2fa708966a9a
Dan Hemberger <daniel.hemberger@gmail.com>
2019-08-27 09:57:14 -0700

    Prepare for next release

--- mechanicalsoup/__version__.py
+++ mechanicalsoup/__version__.py
@@ -2,7 +2,7 @@ __title__ = 'MechanicalSoup'
 __description__ = 'A Python library for automating interaction with websites'
 __url__ = 'https://mechanicalsoup.readthedocs.io/'
 __github_url__ = 'https://github.com/MechanicalSoup/MechanicalSoup'
-__version__ = '0.12.0'
+__version__ = '1.0.0-dev'
 __license__ = 'MIT'
 __github_assets_absoluteURL__ = """\
 https://raw.githubusercontent.com/MechanicalSoup/MechanicalSoup/master"""
