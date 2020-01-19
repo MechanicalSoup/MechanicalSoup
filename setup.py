b8fc87b548e4f03e1dad3a36dde34f7964ef4908
Hugo <hugovk@users.noreply.github.com>
2020-01-19 14:35:07 +0200

    Add python_requires to help pip

--- setup.py
+++ setup.py
@@ -52,6 +52,8 @@ setup(
 
     license=about['__license__'],
 
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+
     classifiers=[
         'License :: OSI Approved :: MIT License',
 
