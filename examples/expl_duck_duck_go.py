bfa0d50677ce4eef7505778668b8537c7f8fb434
Dillonko <dillonko84@gmail.com>
2020-05-02 23:21:02 -0400

    Update expl_duck_duck_go.py
    After testing a few times and debugging found the solution. Documentation should probably be updated

--- examples/expl_duck_duck_go.py
+++ examples/expl_duck_duck_go.py
@@ -8,14 +8,24 @@ DuckDuckGo.
 import mechanicalsoup
 
 # Connect to duckduckgo
-browser = mechanicalsoup.StatefulBrowser()
-browser.open("https://duckduckgo.com/")
+browser = mechanicalsoup.StatefulBrowser(user_agent="MechanicalSoup")
 
-# Fill-in the search form
-browser.select_form('#search_form_input_homepage')
-browser["q"] = "MechanicalSoup"
+# Go to url
+browser.open("https://duckduckgo.com")
+
+# Add value to input box
+browser.get_current_page().find("input").attrs["value"] = "MechanicalSoup"
+
+# find submit button
+submit = browser.get_current_page().find("input", id="search_button_homepage")
+form = browser.select_form()
+
+form.choose_submit(submit)
+
 browser.submit_selected()
 
+
 # Display the results
-for link in browser.page.select('a.result__a'):
-    print(link.text, '->', link.attrs['href'])
+for link in browser.links():
+    print(link.text, "->", link.attrs["href"])
+
