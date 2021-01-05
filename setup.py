a15b6a13635dae9e32422cde80aba9567f99c05a
Dan Hemberger <daniel.hemberger@gmail.com>
2021-01-05 13:07:09 -0800

    Drop support for EOL Python 3.5
    Python 3.5 EOL was on 2020-09-13.

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
 
     license=about['__license__'],
 
-    python_requires='>=3.5',
+    python_requires='>=3.6',
 
     classifiers=[
         'License :: OSI Approved :: MIT License',
@@ -60,7 +60,6 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
