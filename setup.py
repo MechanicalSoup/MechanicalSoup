9d7201d37ededa37a397ecf76f5d4ffd9f48ed4f
Dan Hemberger <846186+hemberger@users.noreply.github.com>
2024-05-06 21:20:16 -0700

    Remove support for python 3.6 and 3.7 (#435)
    These versions reached end of life in Dec 2021 and Jun 2023
    respectively. To address recent security vulnerabilities, we need to
    update to newer versions of dependencies that no longer support these
    EOL versions (e.g. #434, while this is a dev dependency issue only,
    it's probably time to update the production requirements as well).

--- setup.py
+++ setup.py
@@ -69,8 +69,6 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
